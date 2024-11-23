3910b12e5bea8d5162fe3157cd391accf5032983
hrxstik <hisamov2004@gmail.com>
2024-11-23 21:15:08 +0500

    booking page

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/api/images', (req, res) => {
   const imagesDir = path.join(__dirname, 'public/photos');
@@ -22,6 +26,24 @@ app.get('/api/images', (req, res) => {
 
     res.json(images);
   });
+});
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get('/api/bookings', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM bookings');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 app.listen(PORT, () => {
--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const Booking = () => {
-  return <div className="booking page">Бронирование</div>;
+interface Booking {
+  id: number;
+  phone_number: string;
+  start_date: Date;
+  end_date: Date;
+}
+
+const Booking: React.FC = () => {
+  const [bookings, setBookings] = React.useState<Booking[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/bookings');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setBookings(result);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <div className="booking page">{bookings.map((item) => null)}</div>;
 };
 
 export default Booking;
